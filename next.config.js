7c07248c2a784b3a3edfd7a8605226fb05b31c9f
han19950212 <1245595765@qq.com>
2023-12-03 10:32:29 +0800

    fix building error

--- next.config.js
+++ next.config.js
@@ -2,20 +2,19 @@ const withAntdLess = require("next-plugin-antd-less");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["pbs.twimg.com", process.env.NEXT_PUBLIC_APP_URL],
     loader: "custom",
     loaderFile: "./image-loader.ts",
     unoptimized: true,
   },
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_APP_URL + "/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: process.env.NEXT_PUBLIC_APP_URL + "/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = withAntdLess({
